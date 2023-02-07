3cbbbde30bf147b2268712324377c1f32af918d4
MonSter9000999 <124495756+MonSter9000999@users.noreply.github.com>
2023-02-07 20:00:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "YOUR BOT TOKEN",
-    bot_invite_link: "YOUR BOT INVITE LINK",
-    url: "https://join4join.xyz",
-    footer: "discord.gg/join4join",
+    token: "MTA3MjUyNDU0OTUxNTY0NTAyMg.Gisia3.QY2Gl5sjuP4ZYE3S4WMupgLxVBo7mcXwjXTOR0",
+    bot_invite_link: "https://discord.com/api/oauth2/authorize?client_id=1072524549515645022&permissions=8&scope=bot",
+    url: "https://discord.gg/HwgesV6jw2",
+    footer: "https://discord.gg/HwgesV6jw2",
     color: "#e84e4b",
-    admin: [""],
-    owner: [""],
-    api_key: "YOUR API KEY (https://join4join.xyz/profile)",
+    admin: ["959798972124381244"],
+    owner: ["959798972124381244"],
+    api_key: "YOUR API KEY (09a9f7274fd5498bb80d1dd2e71d061c)",
     api_url: "https://join4join.xyz",
 }
